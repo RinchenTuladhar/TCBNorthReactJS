a1f9d4f451fb5b1550421ab294844f957dc25c51
RinchenTuladhar <rinchentul@gmail.com>
2020-05-10 20:59:39 +0100

    Added Firebase API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -15,6 +16,21 @@ import Home from './component/Home';
 import Navbar from './component/Navbar';
 import Login from "./component/auth/Login";
 import Signup from "./component/auth/Signup";
+import * as firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 function App() {
   return (
