a3b26c0cbd4f7077df755d09f0aba5cf910a953b
RinchenTuladhar <rinchentul@gmail.com>
2020-05-18 10:24:23 +0100

    Displayed pages on Edit Page & installed DraftsJS

--- src/component/admin/EditPage.js
+++ src/component/admin/EditPage.js
@@ -7,23 +7,10 @@ class EditPage extends Component{
   constructor(){
     super();
 
-    var temp_nav_items = [];
-
-    firebase.firestore().collection("pages").get().then(function(querySnapshot) {
-      querySnapshot.forEach(function(doc) {
-        var doc_data = doc.data();
-
-        if(doc_data.name !== undefined){
-          temp_nav_items.push(doc_data);
-        }
-      });
-    })
-
     this.state = {
       name: "",
-      child: false,
-      parent_name: "",
-      nav_items: temp_nav_items
+      content: "",
+      nav_items: []
     }
 
     this.handleChange = this.handleChange.bind(this);
@@ -52,32 +39,43 @@ class EditPage extends Component{
     const child = this.state.child;
     const name = this.state.name;
     const parent_name = this.state.child;
+    const url = this.state.url;
+  }
+
+  componentDidMount(){
+    const current_component = this;
+
+    const ref = firebase.firestore().collection("pages")
+    .get()
+    .then(querySnapshot => {
+      const temp_nav_items = [];
+      querySnapshot.forEach(function(doc){
+        const data = doc.data();
+
+        if(data.name !== undefined){
+          temp_nav_items.push(data);
+        }
+      });
+      this.setState({
+        nav_items: temp_nav_items
+      });
+    })
   }
 
   render(){
     return (
       <div className="container squished">
-        <h1>Edit Page</h1>
+      <h1>Edit Page Content</h1>
         <form onSubmit={this.handleSubmit}>
-          <label>Page name
-            {this.state.nav_items.map((item,i) => <p>{item.name}</p>)}
-          </label>
-          <br/><br/>
-          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-          <label>Main navigation?</label>
-          <br/>
-          <label>Yes<input type="radio" id="yes" name="child" value="yes" onChange={this.handleChange} checked/></label>
-          <label>No<input type="radio" id="no" name="child" value="no" onChange={this.handleChange}/></label>
-          <br/>
-          <br/>
-
-          <label>If no, which page does this belong to:
-            <input type="text" name="parent_name" onChange={this.handleChange}/>
-          </label>
-          <br/>
+          <select id="pages">
+          {
+            this.state.nav_items.map(v => {
+              return (
+                <option value={v.name}>{v.name}</option>
+              );
+            })
+          }
+          </select>
           <input type="submit" value="Create"/>
         </form>
       </div>
